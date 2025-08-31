f0c77106331ecf502eca7b2158aef41473adf3ab
Ivan-Marysh <166243361+Ivan-Marysh@users.noreply.github.com>
2025-08-31 19:25:27 +0300

    add 3

--- test.js
+++ test.js
@@ -40,3 +40,110 @@ console.log(countProps(user2));
 console.log("3. Напиши функцію findBestEmployee(employees), яка приймає об'єкт співробітників і повертає ім'я найпродуктивнішого (який виконав більше всіх задач). Співробітники і кількість виконаних завдань містяться як властивості об'єкта в форматі ім'я:кількість задач.");
 
 
+function findBestEmployee(employees) {
+    let bestEmployee = null;
+    let maxTasks = 0;
+
+    for (const [name, tasks] of Object.entries(employees)) {
+        if (tasks > maxTasks) {
+            maxTasks = tasks;
+            bestEmployee = name;
+        }
+    }
+
+    return bestEmployee;
+}
+
+
+const employees = {
+    Matthew: 27,
+    Sanya: 51,
+    Paul: 16,
+    Igor: 34,
+};
+
+console.log(findBestEmployee(employees));
+
+
+
+console.log("4. Напиши функцію countTotalSalary(employees) приймаючу об'єкт зарплат. Функція рахує загальну суму зарплати працівників і повертає її. Кожне поле об'єкта, переданого в функцію, має вигляд ім'я: зарплата.");
+
+
+function countTotalSalary(employees2) {
+    let totalSalary = 0;
+
+    for (const salary of Object.values(employees2)) {
+        totalSalary += salary
+    }
+
+    return totalSalary;
+}
+
+
+const employees2 = {
+    Vyacheslav:  2100,
+    Lubomir:  3000,
+    Volodymer:  2700,
+    Kirill:  3500,
+}
+
+
+console.log(countTotalSalary(employees2));
+
+
+console.log("5. Напиши функцію getAllPropValues(arr, prop), яка отримує масив об'єктів і ім'я властивості. Повертає масив значень певної властивості prop з кожного об'єкта в масиві.");
+
+
+function getAllPropValues(arr, prop) {
+    let values = [];
+
+    for (const obj of arr) {
+        if (obj.hasOwnProperty(prop)) {
+            values.push(obj[prop]);
+        }
+    }
+    return values;
+}
+
+const furniture = [
+    {name: "bed", price: 120, material: "trees"},
+    {name: "chair", price: 65, material: "trees"},
+    {name: "table", price: 90, material: "glass"},
+    {name: "wardrobe", price: 150, material: "trees"},
+]
+
+console.log(getAllPropValues(furniture, "name"));
+console.log(getAllPropValues(furniture, "price"));
+console.log(getAllPropValues(furniture, "material"));
+
+
+
+
+
+console.log("6. Напиши функцію calculateTotalPrice(allProdcuts, productName), яка отримує масив об-єктів та ім-я продукту (значення властивості name). Повертає загальну вартість продукту (ціна * кількість). Викличи функції для перевірки працездатності твоєї реалізації.");
+
+
+
+function calculateTotalPrice(allProducts, productName) {
+    let total = 0;
+
+    for (const product of allProducts) {
+        if (product.name === productName) {
+            total = product.price * product.quantity;
+        }
+    }
+
+    return total;
+}
+
+const products = [
+    { name: "apple", price: 15, quantity: 9 },
+    { name: "banana", price: 10, quantity: 10 },
+    { name: "orange", price: 21, quantity: 6 },
+    { name: "grape", price: 30, quantity: 5 }
+];
+
+console.log(calculateTotalPrice(products, "apple"));
+console.log(calculateTotalPrice(products, "banana"));
+console.log(calculateTotalPrice(products, "orange"));
+console.log(calculateTotalPrice(products, "grape"));
